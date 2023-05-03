683b5cfc68dc506ad8b5a21c61b82a60a323f57b
asadix <asad.hameed@integrationxperts.com>
2023-05-03 10:18:43 +0500

    Asad second fork push.

--- KeyboardSpacer.js
+++ KeyboardSpacer.js
@@ -73,7 +73,7 @@ export default class KeyboardSpacer extends Component {
 
   updateKeyboardSpace(event) {
     if (!event.endCoordinates) {
-      console.log("Ballu");
+      console.log("Ballu shapatar");
       return;
     }
 
